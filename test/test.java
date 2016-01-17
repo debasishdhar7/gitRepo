5d4366c44fe2c4e4710deb6016693dcf399426c7
DebasishDhar <mailtodebasishdhar@gmail.com>
2016-01-17 19:16:48 +0530

    pallindrome program added` <

--- test/test.java
+++ test/test.java
@@ -1,22 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
 
-<title>Contact </title>
-</head>
-body>
-
-lsladalsdaldlasdl;asjdl;ajdla;djl;dadj:
-<h1>New Branh to push testing</h1>
-</body>
-</html> 
-
-
-
-
-
-
-
-
-
-
+import java.util.*;
+ 
+class Palindrome
+{
+   public static void main(String args[])
+   {
+      String original, reverse = "";
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter a string to check if it is a palindrome");
+      original = in.nextLine();
+ 
+      int length = original.length();
+ 
+      for ( int i = length - 1; i >= 0; i-- )
+         reverse = reverse + original.charAt(i);
+ 
+      if (original.equals(reverse))
+         System.out.println("Entered string is a palindrome.");
+      else
+         System.out.println("Entered string is not a palindrome.");
+ 
+   }
+}	
